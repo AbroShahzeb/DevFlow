53e8a763056023bc2b5d76b327ff1354899d4911
AbroShahzeb <khizr772@gmail.com>
2025-03-23 21:38:45 +0500

    Implemented Sign In and Sign Up Form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import React from "react";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  return (
+    <AuthForm
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+      formType="SIGN_UP"
+    />
+  );
 };
 
 export default SignUp;
