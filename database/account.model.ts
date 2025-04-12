2518fb4a35615c4a6124fc4452fdb8244c1d9e11
AbroShahzeb <khizr772@gmail.com>
2025-04-12 17:12:51 +0500

    Implement Account Modal

---
+++ database/account.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema, Types } from "mongoose";
+
+interface IAccount {
+  name: string;
+  provider: string;
+  password?: string;
+  userId: Types.ObjectId;
+  image?: string;
+  providerAccountId: string;
+}
+
+const accountSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    provider: { type: String, required: true },
+    password: { type: String },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    image: { type: String },
+    providerAccountId: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Account = models?.account || model<IAccount>("Account", accountSchema);
+
+export default Account;
