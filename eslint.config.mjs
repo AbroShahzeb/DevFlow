6a68e3bab436d89877739d08111b04f681cc9be5
AbroShahzeb <khizr772@gmail.com>
2025-04-12 12:26:08 +0500

    Implement Question Card

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,46 +7,54 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = {
-    ...compat.extends(
-        "next/core-web-vitals",
-        "next/typescript",
-        "standard",
-        "plugin:tailwindcss/recommended",
-        "prettier"
-    ),
-    plugins: ["import"],
-    "rules": {
-        "import/order": [
-            "error",
-            {
-                "groups": [
-                    "builtin",
-                    "external",
-                    "internal",
-                    ["parent", "sibling"],
-                    "index",
-                    "object"
-                ],
-                "newlines-between": "always",
-                "pathGroups": [
-                    {
-                        "pattern": "@app/**",
-                        "group": "external",
-                        "position": "after"
-                    }
-                ],
-                "pathGroupsExcludedImportTypes": ["builtin"],
-                "alphabetize": {
-                    "order": "asc",
-                    "caseInsensitive": true
-                }
-            }
-        ]
-    }
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  plugins: ["import"],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object",
+        ],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@app/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
 };
 
 export default eslintConfig;
