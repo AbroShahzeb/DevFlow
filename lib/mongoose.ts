62a9558886bec008fcbc8f8ff0bcedc2ea5b3cef
AbroShahzeb <khizr772@gmail.com>
2025-04-12 16:43:08 +0500

    Setup MongoDB and Mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,44 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGO_URI as string;
+
+if (!MONGODB_URI) throw new Error("MONGO_URI is not defined");
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI)
+      .then((result) => {
+        console.log("Connected to DB");
+        return result;
+      })
+      .catch((error) => {
+        console.log("Error connecting to MongoDB", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default dbConnect;
